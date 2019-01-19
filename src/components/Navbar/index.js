5075b8195260c6079321399c2a19f6b0129d392d
himanshu-tiwari <htiwarih@gmail.com>
2019-01-19 17:29:14 +0530

    displaying the current folder int the cnavbar too

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,15 +3,11 @@ import './index.scss';
 import up from '../../assets/icons/navbar-up.png';
 
 const Navbar = (props) => {
-    const { structure, folderUp, parents, peekInFolder } = props;
+    const { structure, folderUp, parents, peekInFolder, currentFolder } = props;
 
     const pathString = parents.map((parent, i) => {
         if (structure.hasOwnProperty(parent)) {
-            if (i === parents.length-1) {
-                return <span className="directory-name" onClick={() => peekInFolder(parent)} key={i}>{ structure[parent].name }</span>;
-            } else {
                 return <span className="directory-name" onClick={() => peekInFolder(parent)} key={i}>{ structure[parent].name } <span className="slash">/ </span></span>;
-            }
         } else {
             return '';
         }
@@ -21,6 +17,7 @@ const Navbar = (props) => {
         <div className="navbar">
             <img src={up} alt="up-icon" onClick={folderUp} className={ !(parents.length > 0) ? 'disabled' : '' } />
             { pathString }
+            <span className="directory-name" onClick={() => peekInFolder(currentFolder)}>{ structure[currentFolder].name }</span>
         </div>
     )
 };
