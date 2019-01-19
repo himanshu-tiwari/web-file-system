a76492c0d630b9faece30009bc4310516d12bda0
himanshu-tiwari <htiwarih@gmail.com>
2019-01-19 14:17:06 +0530

    Disabled sidebar if a modal is open

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,45 +5,41 @@ import down from '../../assets/icons/down.svg';
 const Sidebar = (props) => {
     const { contents } = props.list;
     const displayChildren = (e) => {
-        e.target.closest("ul").classList.toggle("active");
+        e.target.closest("li").classList.toggle("active");
     };
 
     const createStructure = (contents) => {
         return contents.map(folder => {
-            let { id, name, type, contents } = folder;
+            let { id, name, contents } = folder;
             name = typeof(name) === "string" ? name : '';
-            type = typeof(type) === "string" ? type : '';
             contents = typeof(contents) === "object" && contents instanceof Array && contents.length ? contents : [];
 
-            if (type === "folder") {
-                if (contents.length) {
-                    return(
-                        <li key={id}>
-                            <span className="text-span">
-                                { name } <span className="gap"></span> <img className="up" src={down} alt="down" />
-                            </span>
-                            <ul className="internal-directories">
-                                { createStructure(contents) }
-                            </ul>
-                        </li>
-                    );
-                } else {
-                    return(
-                        <li key={id}>
-                            <span className="text-span">
-                                { name }
-                            </span>
-                        </li>
-                    );
-                }
+            if (contents.length) {
+                return(
+                    <li key={id}>
+                        <span className="text-span">
+                            { name } <span className="gap"></span> <img className="up" src={down} alt="down" />
+                        </span>
+                        <ul className="internal-directories">
+                            { createStructure(contents) }
+                        </ul>
+                    </li>
+                );
+            } else {
+                return(
+                    <li key={id}>
+                        <span className="text-span">
+                            { name }
+                        </span>
+                    </li>
+                );
             }
-            return '';
         });
     };
     const listStructure = typeof(contents) === "object" && contents instanceof Array && contents.length ? createStructure(contents) : '';
 
     return(
-        <div className="sidebar">
+        <div className={`sidebar ${props.disabled ? 'disabled' : ''}`}>
             <p className="folder">ROOT</p>
 
             <ul className="directory-structure" onClick={displayChildren}>
